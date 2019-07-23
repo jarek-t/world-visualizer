32dd8e97e82604e164439392cc698e4dcb282d7e
Jarek Troyer <jarektroyer@gmail.com>
2019-07-23 18:39:30 -0400

    Image Converter class added

--- gifCreator.py
+++ gifCreator.py
@@ -1,16 +1,140 @@
-import text_to_image, os, sys, re
+import os, sys, re
 
-sort = lambda fileName: int((re.findall(r'\d+', fileName))[0])
-fileNames = [fn for fn in os.listdir('data')]
-fileNames.sort(key = sort)
-#def getFileNames(dir):
-  #   print(fileNames.sort())
-   # for filename in os.listdir(dir):
-        
-    #    print(filename)
-#for i in range(400):
-#  title = "time_stamp" + str(i) + ".txt"
-#  title2 = "image" + str(i) + ".png"
-#  junk = text_to_image.encode(title, title2)
-print(fileNames)
-#getFileNames('data')
+import imageio
+
+from progress.bar import FillingSquaresBar
+
+from PIL import Image, ImageDraw, ImageFont
+# - # - # - # - # - # - # - # - # - #
+
+class TextVisualizer:
+  sourceLength = False
+  totalY = False
+  titleY = False
+
+  source = False
+  out = False
+
+  background = False
+  write = False
+
+  colors = {
+    "prim": (137, 176, 63),
+    "title": (83, 79, 83)
+  }
+  fonts = {
+    "prim": ImageFont.truetype(os.path.abspath('.') + "/fonts/Roboto-Bold.ttf", size=55),
+    "title": ImageFont.truetype(os.path.abspath('.') + "/fonts/Roboto-Medium.ttf", size=150),
+    "title2": ImageFont.truetype(os.path.abspath('.') + "/fonts/Roboto-Bold.ttf", size=125)
+  }
+
+  def convert(self, source):
+    self.ready(source)
+    
+    count = 0
+    for line in self.source: 
+      yPos = self.titleY + 60 * count
+
+      # print(yPos)
+      location = (50, yPos)
+
+      self.write.text(location, line, font=self.fonts["prim"], fill=self.colors["prim"])
+
+      count += 1
+
+    self.background.save('what.png')
+
+    return self.background
+
+  def ready(self, source):
+    self.source = [line[0:-1] for line in source]
+
+    self.sourceLength = len(self.source)
+
+  def getResult(self): return self.out
+
+  def setSource(self, newSource): self.source = newSource
+
+  def __init__(self, styles = False):
+    self.background = Image.open("bg.png" if not isinstance(styles, dict) or "bg" not in styles else styles["bg"])
+    self.write = ImageDraw.Draw(self.background)
+
+    self.titleY = self.background.size[1] * (10 if not isinstance(styles, dict) or "titleFootprint" not in styles else styles["titleFootprint"])/100
+    
+    for s in ["colors", "fonts"]:
+      if isinstance(styles, dict) and isinstance(styles[s], dict) and s in styles:
+        target = self.colors if s == "colors" else self.fonts
+
+        for key in styles[s]:
+          target[key] = (styles[s])[key]
+
+# - # - # - # - # - # - # - # - # - #
+
+class BatchTextVisualizer:
+  targets = []
+  converted = []
+  out = False 
+
+  writer = TextVisualizer()
+
+  def outOfRange(self, index): 
+    return index < 0 or index > len(self.targets) - 1
+
+  def getTargets(self, dn = False):
+    if not dn: return False
+
+    fileNames = [dn   + "/"+fn for fn in os.listdir(dn)]
+
+    sort = lambda fileName: int((re.findall(r'\d+', fileName))[0])
+    fileNames.sort(key = sort)
+
+    return fileNames
+
+  def convertTarget(self, target, order = False):
+    with open(target, "r") as source:
+      result = self.writer.convert(source.readlines())
+      
+      if order and not self.outOfRange(order):
+        self.converted.insert(order, result)
+
+      source.close()
+
+      return result
+  
+  def convertRange(self, start, end):
+    if not len(self.targets):
+      raise ValueError("There seem to be no targets...")
+    elif self.outOfRange(start) or self.outOfRange(end):
+      raise ValueError("Range does not exist.")
+
+    progress = FillingSquaresBar('Converting', max = (end - start))
+    for t in range(start, end): 
+      
+      img = self.convertTarget(self.targets[t])
+      imgName = './out/'+str(t)+'.png' 
+      img.save(imgName)
+
+      progress.next()
+    progress.finish()
+
+  def convertAll(self): self.convertRange(0, len(self.targets) - 1)
+
+  def export(self):
+    count = 0
+
+    for img in self.converted:
+      img.save("out/"+(str(count) + ".png"))
+
+      count += 1
+  
+  def __init__(self, inDir, outTarget = False, bgFP = "bg.png"):
+    self.targets = self.getTargets(inDir)
+    print("\nFound " + str(len(self.targets)) + " text files to animate.\n")
+
+    self.convertAll()
+
+    self.export()
+  
+# - # - # - # - # - # - # - # - # - #
+
+batch = BatchTextVisualizer('data')
